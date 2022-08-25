1bc608045a573c2f0076f7de019e091adc48798d
Alexander Rodriguez <arodriguez8303@gmail.com>
2022-08-25 12:34:38 -0400

    Added new leash logic to V2

--- DataController.py
+++ DataController.py
@@ -2,39 +2,39 @@ import json
 
 class ConfigSettings:
 
-    def __init__(self, configFileName):
+    def __init__(self, configFileName = None):
         try:
             config = json.load(open(configFileName))
-            print("Successfully read config.")
 
-            self.IP = config["IP"]
-            self.ListeningPort = config["ListeningPort"]
-            self.SendingPort = config["SendingPort"]
-            self.RunDeadzone = config["RunDeadzone"]
-            self.WalkDeadzone = config["WalkDeadzone"]
-            self.ActiveDelay = config["ActiveDelay"]
-            self.InactiveDelay = config["InactiveDelay"]
-            self.Logging = config["Logging"]
+            print("Successfully read config.\n")
+            self.setSettings(config) #Set config values
+
         except Exception as e:
-            print('\x1b[1;31;40m' + 'Missing or incorrect config file. Loading default values.  ' + '\x1b[0m')
-            print("\n", e,"was the exception")
 
-            self.IP = "127.0.0.1"
-            self.ListeningPort = 9001
-            self.SendingPort = 9000
-            self.RunDeadzone = 0.70
-            self.WalkDeadzone = 0.15
-            self.ActiveDelay = .1
-            self.InactiveDelay = .5
-            self.Logging = True
+            print('\x1b[1;31;40m' + 'Missing or incorrect config file. Loading default values.  ' + '\x1b[0m')
+            print(e,"\n")
+
+            self.setSettings() #set default values
+
         
-    def printValues(self):
-        # Settings confirmation
+    def setSettings(self, configJson = None):
+        self.IP = "127.0.0.1" if configJson is None else configJson["IP"]
+        self.ListeningPort = 9001 if configJson is None else configJson["ListeningPort"]
+        self.SendingPort = 9000 if configJson is None else configJson["SendingPort"]
+        self.RunDeadzone = 0.70 if configJson is None else configJson["RunDeadzone"]
+        self.WalkDeadzone = 0.15 if configJson is None else configJson["WalkDeadzone"]
+        self.ActiveDelay = .1 if configJson is None else configJson["ActiveDelay"]
+        self.InactiveDelay = .5 if configJson is None else configJson["InactiveDelay"]
+        self.Logging = True if configJson is None else configJson["Logging"]
+
+    def printInfo(self):        
         print('\x1b[1;32;40m' + 'OSCLeash is Running!' + '\x1b[0m')
+
         if self.IP == "127.0.0.1":
             print("IP: Localhost")
         else:  
             print("IP: Not Localhost? Wack.")
+
         print("Listening on port", self.ListeningPort)
         print("Sending on port", self.SendingPort)
         print("Run Deadzone of {:.0f}".format(self.RunDeadzone*100)+"% stretch")
@@ -42,11 +42,46 @@ class ConfigSettings:
         print("Delays of {:.0f}".format(self.ActiveDelay*1000),"& {:.0f}".format(self.InactiveDelay*1000),"ms")
         #print("Inactive delay of {:.0f}".format(InactiveDelay*1000),"ms")
 
+
+
 class Leash:
-    LeashWasGrabbed: bool = False
-    LeashGrabbed: bool = False
-    LeashStretch: float = 0
-    Z_Positive: float = 0
-    Z_Negative: float = 0
-    X_Positive: float = 0
-    X_Negative: float = 0
+
+    def __init__(self, configFileName = None):
+
+        try:
+            config = json.load(open(configFileName))
+
+            print("Successfully read config.\n")
+            self.getConfigParameters(config) #Set config values
+
+        except Exception as e:
+
+            print('\x1b[1;31;40m' + 'Missing or incorrect config file. Loading default values.  ' + '\x1b[0m')
+            print(e,"\n")
+
+            self.getConfigParameters() #set default values
+        
+    def getConfigParameters(self, configJson = None):
+
+        if configJson is not None:
+            self.configParameters = configJson["Parameters"]
+
+        else: #Load default values
+            self.configParameters = {
+                "Leash_Z+",
+                "Leash_Z-",
+                "Leash_X+",
+                "Leash_Z-",
+                "Leash_IsGrabbed",
+                "Leash_Stretch"
+            }
+
+
+
+    # LeashWasGrabbed: bool = False
+    # LeashGrabbed: bool = False
+    # LeashStretch: float = 0
+    # Z_Positive: float = 0
+    # Z_Negative: float = 0
+    # X_Positive: float = 0
+    # X_Negative: float = 0
