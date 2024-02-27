48c2f2070f4f000ffeeb78f4bf14434f8d331679
ZenithVal <88603991+ZenithVal@users.noreply.github.com>
2024-02-26 22:39:42 -0500

    Dedicated Build Folder

---
+++ Build/1 - Installer.bat
@@ -0,0 +1,2 @@
+@REM This uses CX_Freeze to create an MSI installer file.
+BuildInstaller.py bdist_msi
---
+++ Build/2 - SingleBinary.bat
@@ -0,0 +1,16 @@
+@REM This uses pyinstaller to create a single binary executable. Adjust as needed for your system.
+
+@REM get the repo path from current bat file location and go up one folder
+
+@echo off
+set REPO_PATH=%~dp0..
+set ICON_PATH=%REPO_PATH%\Resources\VRChatOSCLeash.ico
+set SCRIPT_PATH=%REPO_PATH%\OSCLeash.py
+set BUILD_PATH=%REPO_PATH%\Build\dist
+
+pyinstaller --noconfirm --onefile --console --icon "%ICON_PATH%"  "%SCRIPT_PATH%"
+
+@REM Don't need to keep this.
+del "%REPO_PATH%\Build\OSCLeash.spec"
+
+pause
